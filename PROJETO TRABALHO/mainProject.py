06dc213381d35378fed39258b9423a2fe7065dbd
beneficios computações <freitas.lucasaf@gmail.com>
2025-05-26 17:55:50 -0300

    Update mainProject.py

--- PROJETO TRABALHO/mainProject.py
+++ PROJETO TRABALHO/mainProject.py
@@ -5,7 +5,7 @@ import tkinter as tk
 
 # Cores
 purple = "#442e73"
-purpleTwoColor = "#664983"
+lightPurple = "#664983"
 tiffany = "#0ABAB2"
 lightGolden = "#FADA55"
 blue = "#0A7AB2"
@@ -23,7 +23,7 @@ style = ttk.Style()
 
 # Estilo dos botões
 style.configure("Custom.TButton",
-                background=purpleTwoColor,
+                background=lightPurple,
                 foreground="white",
                 font=("Segoe UI", 14, "bold"),
                 padding=20,
@@ -31,17 +31,26 @@ style.configure("Custom.TButton",
 style.map("Custom.TButton",
           background=[("active", "#573a87")])
 
-# Frames (cores aplicadas diretamente, sem estilos)
-frameSelect = tk.Frame(window, width=1300, height=400)
-frameSelect.configure(background=purpleTwoColor)
+style.configure("CustomTwo.TButton",
+                background=tiffany,
+                foreground="white",
+                font=("Segoe UI", 14, "bold"),
+                padding=20,
+                borderwidth=0)
+style.map("CustomTwo.TButton",
+          background=[("active", "#573a87")])
 
-frameInsert = tk.Frame(window, width=1300, height=400)
+# Frames (cores aplicadas diretamente, sem estilos)
+frameSelect = tk.Frame(window, width=1300, height=600)
+frameSelect.configure(background=lightPurple)
+
+frameInsert = tk.Frame(window, width=1300, height=600)
 frameInsert.configure(background=tiffany)
 
-frameUpdate = tk.Frame(window, width=1300, height=400)
+frameUpdate = tk.Frame(window, width=1300, height=600)
 frameUpdate.configure(background=lightGolden)
 
-frameDelete = tk.Frame(window, width=1300, height=400)
+frameDelete = tk.Frame(window, width=1300, height=600)
 frameDelete.configure(background=blue)
 
 frameSelectVisible = False
@@ -115,27 +124,27 @@ line = Separator(window, orient=HORIZONTAL)
 line.grid(row=0, column=0, columnspan=2, sticky="ew", padx=20, pady=30)
 
 # Linha vertical
-canvasLine = tk.Canvas(window, width=10, height=400, bg=purple, highlightthickness=0)
+canvasLine = tk.Canvas(window, width=10, height=420, bg=purple, highlightthickness=0)
 canvasLine.grid(row=1, column=1, rowspan=4, sticky="ns", padx=10, pady=10)
 
-def roundedRect(canvas, x1, y1, x2, y2, radius=5, **kwargs):
-    points = [
-        x1+radius, y1,
-        x2-radius, y1,
-        x2, y1,
-        x2, y1+radius,
-        x2, y2-radius,
-        x2, y2,
-        x2-radius, y2,
-        x1+radius, y2,
-        x1, y2,
-        x1, y2-radius,
-        x1, y1+radius,
-        x1, y1,
-    ]
-    return canvas.create_polygon(points, **kwargs, smooth=True)
+# Função para linha arredondada em canvas
+def drawRoundedRect(canvas, x, y, width, height, color):
+    radius = width // 2
+    canvas.create_oval(x, y, x + width, y + width, fill=color, outline="")
+    canvas.create_rectangle(x, y + radius, x + width, y + height - radius, fill=color, outline="")
+    canvas.create_oval(x, y + height - width, x + width, y + height, fill=color, outline="")
 
-roundedRect(canvasLine, 0, 0, 10, 1000, radius=5, fill=white, outline="")
+drawRoundedRect(canvasLine, 0,0 ,10, 559, white)
+
+# INFORMAÇÃO DO JOGO
+
+labelGameInformation = ttk.Label(frameSelect, text="Informação do Jogo", background=lightPurple, font=("Times New Roman", 20))
+labelGameInformation.grid(row=0, column=0, columnspan=3, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+# INFORMAÇÕES DO USUÁRIO
+
+labelPlayerInformation = ttk.Label(frameSelect, text="Informação do Usuário", background=lightPurple, font=("Times New Roman", 20))
+labelPlayerInformation.grid(row=3, column=0, columnspan=3, pady=20, padx=20, sticky=NSEW, ipady=20)
 
 # Botões
 buttonSelect = ttk.Button(window, text="Select", style="Custom.TButton", command=selectFrame)
@@ -149,6 +158,37 @@ buttonUpdate.grid(row=3, column=0, pady=20, padx=20, sticky=NSEW, ipady=20)
 
 buttonDelete = ttk.Button(window, text="Delete", style="Custom.TButton", command=deleteFrame)
 buttonDelete.grid(row=4, column=0, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+# FUNÇÃO DO BOTÃO SELECT
+
+# BOTÕES COM FUNÇÕES PARA PROCURAR INFORMAÇÕES DOS JOGOS
+buttonSearchIDGame = ttk.Button(frameSelect, text="ID do Jogo", style="CustomTwo.TButton", command=selectFrame)
+buttonSearchIDGame.grid(row=1, column=0, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+buttonSearchNameGame = ttk.Button(frameSelect, text="Nome", style="CustomTwo.TButton", command=selectFrame)
+buttonSearchNameGame.grid(row=1, column=1, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+buttonSearchGenderGame = ttk.Button(frameSelect, text="Gênero", style="CustomTwo.TButton", command=selectFrame)
+buttonSearchGenderGame.grid(row=1, column=2, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+buttonSearchPriceGame = ttk.Button(frameSelect, text="Preço", style="CustomTwo.TButton", command=selectFrame)
+buttonSearchPriceGame.grid(row=1, column=3, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+buttonSearchProducerGame = ttk.Button(frameSelect, text="Produtor", style="CustomTwo.TButton", command=selectFrame)
+buttonSearchProducerGame.grid(row=1, column=4, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+buttonSearchAgeGame = ttk.Button(frameSelect, text="Idade", style="CustomTwo.TButton", command=selectFrame)
+buttonSearchAgeGame.grid(row=1, column=5, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+# BOTÕES PARA PROCURAR INFORMAÇÕES DO USUÁRIO
+
+buttonSearchNamePlayer = ttk.Button(frameSelect, text="Nome", style="CustomTwo.TButton", command=selectFrame)
+buttonSearchNamePlayer.grid(row=8, column=0, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+buttonSearchIDPlayer = ttk.Button(frameSelect, text="ID do Usuário", style="CustomTwo.TButton", command=selectFrame)
+buttonSearchIDPlayer.grid(row=8, column=1, pady=20, padx=20, sticky=NSEW, ipady=20)
+
+# BOTÃO PARA PROCURAR INFORMAÇÃO NO CARRINHO
 
 # Loop principal
 window.mainloop()
