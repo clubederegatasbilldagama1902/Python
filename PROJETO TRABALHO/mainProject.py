04ba9b6412c81a1a03a36fa9927ff5d72e5cd1c4
beneficios computações <freitas.lucasaf@gmail.com>
2025-06-09 15:58:50 -0300

    Update mainProject.py

--- PROJETO TRABALHO/mainProject.py
+++ PROJETO TRABALHO/mainProject.py
@@ -83,6 +83,8 @@ frame_insert_visible = False
 frame_update_visible = False
 frame_delete_visible = False
 frame_cart_visible = False
+frame_game_visible = False
+frame_player_visible = False
 
 # -----------------------> FUNÇÕES PARA GERENCIAR FRAMES <-----------------------
 
@@ -180,6 +182,95 @@ def delete_frame():
         frame_cart_visible = False
 
 
+def game_frame():
+    """Controla a exibição do frame de informações de jogos"""
+    global frame_game_visible, frame_player_visible, \
+        label_entry_id_game, entry_id_game, \
+        label_entry_name_game, entry_name_game, \
+        label_entry_gender_game, entry_gender_game, \
+        label_entry_price_game, entry_price_game, \
+        label_entry_producer_game, entry_producer_game, \
+        label_entry_age_game, entry_age_game
+
+    if frame_game_visible:
+        label_entry_id_game.place_forget()
+        label_entry_name_game.place_forget()
+        label_entry_gender_game.place_forget()
+        label_entry_price_game.place_forget()
+        label_entry_producer_game.place_forget()
+        label_entry_age_game.place_forget()
+
+        entry_id_game.place_forget()
+        entry_name_game.place_forget()
+        entry_gender_game.place_forget()
+        entry_price_game.place_forget()
+        entry_producer_game.place_forget()
+        entry_age_game.place_forget()
+
+        frame_game_visible = False
+    else:
+        label_entry_id_game.place(x=30, y=165)
+        label_entry_name_game.place(x=190,y=165)
+        label_entry_gender_game.place(x=350, y=165)
+        label_entry_price_game.place(x=510,y=165)
+        label_entry_producer_game.place(x=670, y=165)
+        label_entry_age_game.place(x=830, y=165)
+
+        entry_id_game.place(x=30, y=200)
+        entry_name_game.place(x=190,y=200)
+        entry_gender_game.place(x=350, y=200)
+        entry_price_game.place(x=510,y=200)
+        entry_producer_game.place(x=670, y=200)
+        entry_age_game.place(x=830, y=200)
+
+        frame_game_visible = True
+        frame_player_visible = False
+
+def player_frame():
+    """Controla a exibição do frame de informações de jogadores"""
+    global frame_player_visible, frame_game_visible, \
+        label_entry_id_player, entry_id_player, \
+        label_entry_name_player, entry_name_player, \
+        label_entry_age_player, entry_age_player, \
+        label_entry_country_player, entry_country_player, \
+        label_entry_status_player, entry_status_player, \
+        label_entry_games_created_player, entry_games_created_player
+
+    if frame_player_visible:
+        label_entry_id_player.place_forget()
+        label_entry_name_player.place_forget()
+        label_entry_age_player.place_forget()
+        label_entry_country_player.place_forget()
+        label_entry_status_player.place_forget()
+        label_entry_games_created_player.place_forget()
+
+        entry_id_player.place_forget()
+        entry_name_player.place_forget()
+        entry_age_player.place_forget()
+        entry_country_player.place_forget()
+        entry_status_player.place_forget()
+        entry_games_created_player.place_forget()
+
+        frame_player_visible = False
+    else:
+        label_entry_id_player.place(x=30, y=165)
+        label_entry_name_player.place(x=190,y=165)
+        label_entry_age_player.place(x=350, y=165)
+        label_entry_country_player.place(x=510,y=165)
+        label_entry_status_player.place(x=670, y=165)
+        label_entry_games_created_player.place(x=830, y=165)
+
+        entry_id_player.place(x=30,y=200)
+        entry_name_player.place(x=190,y=200)
+        entry_age_player.place(x=350, y=200)
+        entry_country_player.place(x=510,y=200)
+        entry_status_player.place(x=670, y=200)
+        entry_games_created_player.place(x=830, y=200)
+
+        frame_player_visible = True
+        frame_game_visible = False
+
+
 # -----------------------> ELEMENTOS VISUAIS ADICIONAIS <-----------------------
 
 # Cria uma linha vertical decorativa com extremidades arredondadas
@@ -226,6 +317,134 @@ def draw_rounded_line(canvas, x, y1, y2, width, color):
 # Aplica a função para desenhar a linha
 draw_rounded_line(canvas_line, 0, 0, 489, 10, light_lilac)
 
+# -----------------------> CAMPOS DE ENTRADA NO FRAME DE JOGOS <-----------------------
+
+# Label para o campo de ID do Jogo
+label_entry_id_game = ttk.Label(frame_insert,
+                               text="ID Jogo",
+                               background=light_purple,
+                               font=("Calibri", 20))
+
+# Campo de entrada para o ID do Jogo
+entry_id_game = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_id_game.configure(bg="#C6B4C9")
+
+# Label para o campo de Nome do Jogo
+label_entry_name_game = ttk.Label(frame_insert,
+                                 text="Nome",
+                                 background=light_purple,
+                                 font=("Calibri", 20))
+
+# Campo de entrada para o Nome do Jogo
+entry_name_game = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_name_game.configure(bg="#C6B4C9")
+
+# Label para o campo de Gênero do Jogo
+label_entry_gender_game = ttk.Label(frame_insert,
+                                  text="Gênero",
+                                  background=light_purple,
+                                  font=("Calibri", 20))
+
+# Campo de entrada para o Gênero do Jogo
+entry_gender_game = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_gender_game.configure(bg="#C6B4C9")
+
+# Label para o campo de Preço do Jogo
+label_entry_price_game = ttk.Label(frame_insert,
+                                 text="Preço",
+                                 background=light_purple,
+                                 font=("Calibri", 20))
+
+# Campo de entrada para o Preço do Jogo
+entry_price_game = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_price_game.configure(bg="#C6B4C9")
+
+# Label para o campo de Produtora do Jogo
+label_entry_producer_game = ttk.Label(frame_insert,
+                                    text="Produtora",
+                                    background=light_purple,
+                                    font=("Calibri", 20))
+
+# Campo de entrada para a Produtora do Jogo
+entry_producer_game = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_producer_game.configure(bg="#C6B4C9")
+
+# Label para o campo de Idade indicada do Jogo
+label_entry_age_game = ttk.Label(frame_insert,
+                               text="Idade",
+                               background=light_purple,
+                               font=("Calibri", 20))
+
+# Campo de entrada para a Idade indicada do Jogo
+entry_age_game = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_age_game.configure(bg="#C6B4C9")
+
+
+
+
+
+
+
+# Label para o campo de ID do Usuário (Jogador)
+label_entry_id_player = ttk.Label(frame_insert,
+                                text="ID do Usuário",
+                                background=light_purple,
+                                font=("Calibri", 20))
+
+# Campo de entrada para o ID do Usuário
+entry_id_player = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_id_player.configure(bg="#C6B4C9")
+
+# Label para o campo de Nome do Usuário
+label_entry_name_player = ttk.Label(frame_insert,
+                                  text="Nome",
+                                  background=light_purple,
+                                  font=("Calibri", 20))
+
+# Campo de entrada para o Nome do Usuário
+entry_name_player = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_name_player.configure(bg="#C6B4C9")
+
+# Label para o campo de Idade do Usuário
+label_entry_age_player = ttk.Label(frame_insert,
+                                 text="Idade",
+                                 background=light_purple,
+                                 font=("Calibri", 20))
+
+# Campo de entrada para a Idade do Usuário
+entry_age_player = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_age_player.configure(bg="#C6B4C9")
+
+# Label para o campo de País do Usuário
+label_entry_country_player = ttk.Label(frame_insert,
+                                      text="País",
+                                      background=light_purple,
+                                      font=("Calibri", 20))
+
+# Campo de entrada para o País do Usuário
+entry_country_player = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_country_player.configure(bg="#C6B4C9")
+
+# Label para o campo de Status do Usuário
+label_entry_status_player = ttk.Label(frame_insert,
+                                    text="Status",
+                                    background=light_purple,
+                                    font=("Calibri", 20))
+
+# Campo de entrada para o Status do Usuário
+entry_status_player = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_status_player.configure(bg="#C6B4C9")
+
+# Label para o campo de quantidade de Jogos Criados pelo Usuário
+label_entry_games_created_player = ttk.Label(frame_insert,
+                                           text="Jogos Criados",
+                                           background=light_purple,
+                                           font=("Calibri", 20))
+
+# Campo de entrada para a quantidade de Jogos Criados pelo Usuário
+entry_games_created_player = tk.Entry(frame_insert, width=20, bg=purple, fg=light_purple, font=("Calibri", 10))
+entry_games_created_player.configure(bg="#C6B4C9")
+
 # -----------------------> INÍCIA O LOOP PRINCIPAL DA INTERFACE <-----------------------
 
 if __name__ == "__main__":
@@ -244,7 +463,9 @@ if __name__ == "__main__":
         select_frame,  # Função para mostrar frame de consulta
         insert_frame,  # Função para mostrar frame de inserção
         update_frame,  # Função para mostrar frame de atualização
-        delete_frame  # Função para mostrar frame de exclusão
+        delete_frame,  # Função para mostrar frame de exclusão
+        game_frame,
+        player_frame
     )
 
     # Criação dos botões na interface
